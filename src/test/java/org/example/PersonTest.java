efccdaf37a45077722d9029b2ff56402c7afe749
fdw <florina@danciu-wendt.net>
2020-09-24 20:19:43 +0200

    ArrayList added

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -4,46 +4,66 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
-public class PersonTest
-{
+public class PersonTest {
 
     @Test
-    public void arrayOfPersons()
-    {
+    public void arrayOfPersons() {
         Person[] people = new Person[3];
 
-        for ( int i = 0; i < people.length; i++) {
+        for (int i = 0; i < people.length; i++) {
             people[i] = new Person("Person" + i);
         }
 
-        for ( int i = 0; i < people.length; i++) {
+        for (int i = 0; i < people.length; i++) {
             System.out.println(people[i]);
         }
 
-        for(Person person: people ) {
+        for (Person person : people) {
             System.out.println(person);
         }
     }
 
     @Test
-    public void addDifferentPersonTypes () {
+    public void addDifferentPersonTypes() {
 
         Person[] people = new Person[11];
 
-        for ( int i = 0; i < people.length; i++) {
+        for (int i = 0; i < people.length; i++) {
             if (i % 3 == 0) {
                 people[i] = new Person("Person" + i);
-            } else if ( i % 3 == 1) {
+            } else if (i % 3 == 1) {
                 people[i] = new Employee("Person" + i);
             } else {
                 people[i] = new Unemployed("Person" + i);
             }
         }
 
-        for(Person person: people ) {
+        for (Person person : people) {
+            System.out.println(person);
+        }
+    }
+@Test
+    public void addDifferentPersonTypesInArrayList() {
+
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < 11; i++) {
+            if (i % 3 == 0) {
+                people.add(new Person("Person" + i));
+            } else if (i % 3 == 1) {
+                people.add(new Employee("Person" + i));
+            } else {
+                people.add(new Unemployed("Person" + i));
+            }
+        }
+
+        for (Person person : people) {
             System.out.println(person);
         }
     }
